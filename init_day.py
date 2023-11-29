b7a19d42efe3e1bda4936ab6c9770b52c9cd5c72
Joshua Foster <josh2112@gmail.com>
2023-11-29 17:25:16 -0500

    Initial check-in.

---
+++ init_day.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+import argparse
+import datetime
+import os
+
+from dotenv import load_dotenv
+
+import requests
+
+load_dotenv()
+
+AOC_SESSION = os.getenv("AOC_SESSION")
+if not AOC_SESSION:
+    raise EnvironmentError("Error: AOC_SESSION not found in environment or .env file")
+
+
+def init_input(year: int, day: int, path: str) -> bool:
+    url = f"https://adventofcode.com/{year}/day/{day}/input"
+    headers = {"Cookie": f"session={AOC_SESSION}"}
+    r = requests.get(url, headers=headers, timeout=5)
+
+    if r.status_code != 200:
+        raise IOError(f"Error getting input: HTTP {r.status_code}: {r.reason} \n{r.text}")
+
+    with open(path, mode="w", encoding="utf-8") as f:
+        f.write(r.text)
+
+    return True
+
+
+def init_script(path: str) -> bool:
+    if os.path.exists(path):
+        print(f"Warning: {path} already exists, skipping")
+        return False
+
+    with open(path, "x", encoding="utf-8") as dest:
+        with open("solve.template.py", "r", encoding="utf-8") as src:
+            dest.write(src.read())
+
+    os.chmod(path, 0o744)
+    return True
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Sets up daily Advent of Code problem",
+        epilog="Must supply browser session id in AOC_SESSION env var or in .env file",
+    )
+    date = datetime.date.today()
+    parser.add_argument("-y", "--year", default=date.year)
+    parser.add_argument("-d", "--day", default=date.day)
+    args = parser.parse_args()
+    year = args.year
+    day = args.day
+
+    path = os.path.join(year, day)
+    os.makedirs(path, exist_ok=True)
+
+    input_path = os.path.join(path, "input.txt")
+    if init_input(year, day, input_path):
+        print(f"Wrote input: {input_path}")
+
+    script_path = os.path.join(path, "solve.py")
+    if init_script(script_path):
+        print(f"Wrote script: {script_path}")
+
+
+if __name__ == "__main__":
+    main()
