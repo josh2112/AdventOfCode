c4465b6618a86261029cd4f793af19e6bdf0977a
Joshua Foster <josh2112@gmail.com>
2024-01-03 16:16:57 -0500

    day 23 part 2: YES SOLVED!

--- 2023/23/solve.py
+++ 2023/23/solve.py
@@ -4,7 +4,7 @@ from collections import defaultdict
 from itertools import pairwise
 import time
 
-from aoclib.direction import Direction
+from aoclib.direction import Direction, XYPos
 
 # https://adventofcode.com/2023/day/23
 
@@ -15,37 +15,31 @@ INPUT = "input.txt"
 PART = 2
 
 Nodes = dict[int, dict[int, int]]
+Nodes2 = dict[XYPos, dict[XYPos, int]]
 
 
-def build_graph(data: list[str], ignore_directional: bool) -> Nodes:
-    def xy2int(xy: tuple[int, int]) -> int:
-        return (xy[0] << 8) + xy[1]
-
-    def int2xy(i: int) -> tuple[int, int]:
-        return i >> 8, i & 0xFF
-
+def build_graph(data: list[str]) -> Nodes2:
     # look up by (x,y) instead of [y][x]
     maze = {(x, y): data[y][x] for y in range(len(data)) for x in range(len(data[y]))}
 
     # position -> neighbor node positions with values = path lengths
-    nodes: Nodes = defaultdict(dict)
+    nodes: Nodes2 = defaultdict(dict)
 
     # start, direction, is_bidi
-    Hallway = tuple[int, Direction, bool]
+    Hallway = tuple[XYPos, Direction, bool]
     # start at (1,0) moving down
-    hallways: list[Hallway] = [(xy2int((1, 0)), Direction.DOWN, True)]
+    hallways: list[Hallway] = [((1, 0), Direction.DOWN, True)]
 
     while hallways:
         start, direction, is_bidi = hallways.pop(0)
-        s = int2xy(start)
 
         # Take advantage of the fact that a node can have at most one
         # neighbor in each direction. If the (start) node has a neighbor
         # in (direction) already, skip it.
-        if any(Direction.from_line(s, int2xy(n)) == direction for n in nodes[start]):
+        if any(Direction.from_line(start, n) == direction for n in nodes[start]):
             continue
 
-        pos = s
+        pos = start
 
         while True:
             # Move forward 1
@@ -54,14 +48,13 @@ def build_graph(data: list[str], ignore_directional: bool) -> Nodes:
             if pos not in maze:
                 # We must be at the exit
                 pos_prev = direction.advance(pos, -1)
-                length = abs(s[0] - pos_prev[0]) + abs(s[1] - pos_prev[1])
-                pp = xy2int(pos_prev)
-                nodes[start][pp] = length
-                nodes[pp][start] = length
+                length = abs(start[0] - pos_prev[0]) + abs(start[1] - pos_prev[1])
+                nodes[start][pos_prev] = length
+                nodes[pos_prev][start] = length
                 break
 
             # Enforce directionality arrow
-            if not ignore_directional and (arrow := "^>v<".find(maze[pos])) != -1:
+            if (arrow := "^>v<".find(maze[pos])) != -1:
                 if arrow == direction:
                     # Now we know it's one-way
                     is_bidi = False
@@ -77,31 +70,117 @@ def build_graph(data: list[str], ignore_directional: bool) -> Nodes:
 
             if can_go_right or can_go_left:
                 # Create bidirectional connection
-                length = abs(s[0] - pos[0]) + abs(s[1] - pos[1])
-                p = xy2int(pos)
-                nodes[start][p] = length
+                length = abs(start[0] - pos[0]) + abs(start[1] - pos[1])
+                nodes[start][pos] = length
                 if is_bidi:
-                    nodes[p][start] = length
+                    nodes[pos][start] = length
 
                 # Create new hallways left and/or right
                 if can_go_right:
-                    hallways.append((p, dir_right, True))
+                    hallways.append((pos, dir_right, True))
                 if can_go_left:
-                    hallways.append((p, dir_left, True))
+                    hallways.append((pos, dir_left, True))
 
                 # Start a new hallway forward if we can
                 if maze[direction.advance(pos)] != "#":
-                    hallways.append((p, direction, True))
+                    hallways.append((pos, direction, True))
 
                 break
 
     return nodes
 
 
-def walk(nodes: Nodes, initial_path: list[int], goal: int) -> int:
+# Build a condensed version of the graph, condensing long hallways with no exits.
+# Only use for part 2 (breaks the directionality arrows!)
+def build_graph_2(data: list[str]) -> Nodes2:
+    def xy2int(xy: tuple[int, int]) -> int:
+        return (xy[0] << 8) + xy[1]
+
+    def int2xy(i: int) -> tuple[int, int]:
+        return i >> 8, i & 0xFF
+
+    # look up by (x,y) instead of [y][x]
+    maze = {(x, y): data[y][x] for y in range(len(data)) for x in range(len(data[y]))}
+
+    # position -> neighbor node positions with values = path lengths
+    nodes: Nodes2 = defaultdict(dict)
+
+    # start, direction
+    Path = tuple[XYPos, Direction]
+    # start at (1,0) moving down
+    paths: list[Path] = [((1, 0), Direction.DOWN)]
+
+    while paths:
+        start, direction = paths.pop(0)
+
+        # Take advantage of the fact that a node can have at most one
+        # neighbor in each direction. If the (start) node has a neighbor
+        # in (direction) already, skip it.
+        # if any(Direction.from_line(start, n) == direction for n in nodes[start]):
+        #    continue
+
+        pos = start
+        length = 0
+
+        while True:
+            # Move forward 1
+            pos = direction.advance(pos)
+            length += 1
+
+            if pos not in maze:
+                # We must be at the exit
+                pos_prev = direction.advance(pos, -1)
+                length = abs(start[0] - pos_prev[0]) + abs(start[1] - pos_prev[1])
+                nodes[start][pos_prev] = length
+                nodes[pos_prev][start] = length
+                break
+
+            # Can we turn right and/or left here?
+            dir_right = direction.turn(1)
+            can_go_right = maze[dir_right.advance(pos)] != "#"
+            dir_left = direction.turn(-1)
+            can_go_left = maze[dir_left.advance(pos)] != "#"
+            can_go_forward = (newpos := direction.advance(pos)) in maze and maze[
+                newpos
+            ] != "#"
+
+            num_choices = sum(
+                1 for d in (can_go_left, can_go_right, can_go_forward) if d
+            )
+
+            if num_choices == 1:
+                # There's only one way to advance. Change direction if needed, then keep going
+                if can_go_right:
+                    direction = dir_right
+                elif can_go_left:
+                    direction = dir_left
+            else:
+                # Dead-end or junction point
+
+                if pos in nodes[start]:
+                    # We've already seen this node; stop!
+                    break
+
+                # Create bidirectional connection
+                nodes[start][pos] = length
+                nodes[pos][start] = length
+
+                # Create new hallways left, right and/or straight
+                if can_go_right:
+                    paths.append((pos, dir_right))
+                if can_go_left:
+                    paths.append((pos, dir_left))
+                if can_go_forward:
+                    paths.append((pos, direction))
+                break
+
+    return nodes
+
+
+def walk(nodes: Nodes2, start: XYPos, goal: XYPos) -> int:
     time_start = time.perf_counter()
     max_steps = 0
-    paths: list[list[int]] = [initial_path]
+    paths: list[list[XYPos]] = [[start]]
 
     while paths:
         path = paths.pop(0)
@@ -127,102 +206,30 @@ def walk(nodes: Nodes, initial_path: list[int], goal: int) -> int:
     return max_steps
 
 
-def collapse_non_branches_1(nodes: Nodes):
-    already_processed = []
-    # Try to find long segments with no branches and collapse them
-    n = next((n for n in nodes if len(nodes[n]) == 2), None)
-    while n:
-        # Find the ends of the chain
-        s, e = n, n
-        length = 0
-        to_remove = set([n])
-        while len(nodes[tmp := list(nodes[s])[0]]) == 2:
-            length += nodes[s][tmp]
-            to_remove.add(tmp)
-            s = tmp
-        while len(nodes[tmp := list(nodes[e])[1]]) == 2:
-            length += nodes[e][tmp]
-            to_remove.add(tmp)
-            e = tmp
-        if s != e:
-            del nodes[s][next(n for n in nodes[s] if n in to_remove)]
-            del nodes[e][next(n for n in nodes[e] if n in to_remove)]
-            nodes[s][e] = length
-            nodes[e][s] = length
-
-            for n in [n for n in to_remove if n not in [s, e]]:
-                del nodes[n]
-
-            already_processed.append(s)
-        already_processed.append(e)
-
-        n = next(
-            (n for n in nodes if n not in already_processed and len(nodes[n]) == 2),
-            None,
-        )
-        pass
-
-
-def collapse_non_branches_2(nodes: Nodes):
-    # Try to find long segments with no branches and collapse them
-    n = next((n for n in nodes if len(nodes[n]) == 2), None)
-    while n:
-        # Find the ends of the chain
-        s, e = n, n
-        length = 0
-        to_remove = set([n])
-        while len(nodes[s]) == 2:
-            tmp = list(nodes[s])[0]
-            length += nodes[s][tmp]
-            to_remove.add(tmp)
-            s = tmp
-        while len(nodes[e]) == 2:
-            tmp = list(nodes[e])[1]
-            length += nodes[e][tmp]
-            to_remove.add(tmp)
-            e = tmp
-        # s = (1,1), e = (4,1)
-        del nodes[s][next(n for n in nodes[s] if n in to_remove)]
-        del nodes[e][next(n for n in nodes[e] if n in to_remove)]
-        nodes[s][e] = length
-        nodes[e][s] = length
-
-        for n in [n for n in to_remove if n not in [s, e]]:
-            del nodes[n]
-
-        n = next((n for n in nodes if len(nodes[n]) == 2), None)
-        pass
-
-
-def longest_path(data: list[str], ignore_directionals: bool):
-    def xy2int(xy: tuple[int, int]) -> int:
-        return (xy[0] << 8) + xy[1]
-
-    nodes = build_graph(data, ignore_directionals)
-    # collapse_non_branches_1(nodes)
+def longest_path(data: list[str]):
+    nodes = build_graph_2(data) if PART == 2 else build_graph(data)
 
     for n in nodes.keys():
         nodes[n] = dict(sorted(nodes[n].items(), key=lambda kv: kv[1], reverse=True))
 
-    path = [xy2int((1, 0)), xy2int((1, 1))]
-    goal = xy2int((len(data[0]) - 2, len(data) - 1))
+    start = (1, 0)
+    goal = (len(data[0]) - 2, len(data) - 1)
     # Optimization: If we hit the node directly above the exit, we must take the exit,
     # otherwise we'll block our path to it. So make the goal the penultimate node, and
     # just add the last-hop size later.
     penultimate = next(iter(nodes[goal]))
     print(f"* Add {nodes[penultimate][goal]} for last leg!")
 
-    max_steps = walk(nodes, path, penultimate)
+    max_steps = walk(nodes, start, penultimate)
     return max_steps + nodes[penultimate][goal]
 
 
 def prob_1(data: list[str]):
-    return longest_path(data, False)
+    return longest_path(data)
 
 
-# TODO: It's bigger than 6226...
 def prob_2(data: list[str]):
-    return longest_path(data, True)
+    return longest_path(data)
 
 
 def main():
