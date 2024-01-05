baee6760840413d0e02fe07b3fc03c1d2bcc4ab0
Joshua Foster <josh2112@gmail.com>
2024-01-05 15:30:20 -0500

    got day 23 part 2!

--- 2023/21/solve.py
+++ 2023/21/solve.py
@@ -12,7 +12,7 @@ from PIL import Image
 INPUT = "input.txt"
 
 # Part to solve, 1 or 2
-PART = 1
+PART = 2
 
 
 @dataclasses.dataclass(frozen=True)
@@ -34,8 +34,26 @@ class Garden:
                     start = (x, y)
         return Garden(len(data[0]), len(data), frozenset(rocks), start)
 
+    def tiled(self, size: int):
+        rocks = []
+        for x in range(size):
+            for y in range(size):
+                rocks.extend(
+                    (r[0] + self.xmax * x, r[1] + self.ymax * y) for r in self.rocks
+                )
+
+        return Garden(
+            self.xmax * size,
+            self.ymax * size,
+            frozenset(rocks),
+            (
+                self.xmax * (size // 2) + self.start[0],
+                self.ymax * (size // 2) + self.start[1],
+            ),
+        )
+
     @functools.cache
-    def neighbors_1(self, u: tuple[int, int]):
+    def neighbors(self, u: tuple[int, int]):
         coords = set()
         for v in (
             (u[0] - 1, u[1]),
@@ -53,28 +71,15 @@ class Garden:
                 coords.add(v)
         return coords
 
-    @functools.cache
-    def neighbors_2(self, u: tuple[int, int]):
-        coords = defaultdict(set)
-        for v in (
-            (u[0] - 1, u[1]),
-            (u[0], u[1] - 1),
-            (u[0] + 1, u[1]),
-            (u[0], u[1] + 1),
-        ):
-            vnorm = (v[0] % self.xmax, v[1] % self.ymax)
-            if vnorm not in self.rocks:
-                coords[(v[0] // self.xmax, v[1] // self.ymax)].add(vnorm)
-        return coords
 
-
-def save_img(path: str, g: Garden, coords: set[tuple[int, int]]):
+def show_img(path: str, g: Garden, coords: set[tuple[int, int]]):
     img = Image.new(mode="RGB", size=(g.xmax, g.ymax))
     for rock in g.rocks:
         img.putpixel(rock, (128, 128, 128))
     for coord in coords:
         img.putpixel(coord, (0, 255, 85))
     img.save(path, format="PNG")
+    img.show()
 
 
 def prob_1(data: list[str]):
@@ -84,7 +89,7 @@ def prob_1(data: list[str]):
     for i in range(64):
         newstack = set()
         for u in stack:
-            newstack.update(g.neighbors_1(u))
+            newstack.update(g.neighbors(u))
         stack = newstack
         print(f"{i+1}: {len(stack)}")
 
@@ -99,22 +104,61 @@ def prob_1(data: list[str]):
 # divisible by 131 (result 202300). So the walk extends to each border of the center, then goes out
 # 202300 more 'plots' in all four directions.
 #
-# This also puts the frontier in each 'plot' right in the middle of the big diamond-shaped "valley"
-#
+# It ends up making a giant multi-plot diamond. The plots are pretty regular -- there's a plot
+# representing each "point", two alternating plots that make up each edge, and 2 alternating center
+# plots. We just have to make the smallest multi-plot diamond we can to get all the various tile
+# plot counts, figure out how many of each there would be in a 202300-radius multi-plot diamond,
+# then add them all up.
 def prob_2(data: list[str]):
+    width = 5
+
     g = Garden.parse(data)
-    stack = defaultdict(set)
-    stack[(0, 0)].add(g.start)
+    plotsize = g.xmax
 
-    for i in range(20):
-        newstack = defaultdict(set)
-        for grid, coords in stack.items():
-            for c in coords:
-                newstack.update(g.neighbors_2(c))
-        stack = newstack
-        print(f"{i+1}: {[len(stack[g]) for g in stack]}")
+    g = g.tiled(width)
+    tiles: set[tuple[int, int]] = set([g.start])
 
-    return len(stack)
+    for _ in range(plotsize * (width // 2) + 65):
+        next_tiles = set()
+        for u in tiles:
+            next_tiles.update(g.neighbors(u))
+        tiles = next_tiles
+
+    def count_tiles_in_plot(xy: tuple[int, int]):
+        x0, x1 = xy[0] * plotsize, (xy[0] + 1) * plotsize
+        y0, y1 = xy[1] * plotsize, (xy[1] + 1) * plotsize
+        return sum(1 for t in tiles if x0 <= t[0] < x1 and y0 <= t[1] < y1)
+
+    half = width // 2
+
+    t, l, r, b = map(
+        count_tiles_in_plot,
+        ((half, 0), (0, half), (width - 1, half), (half, width - 1)),
+    )
+    lto, rto, lbo, rbo = map(
+        count_tiles_in_plot,
+        ((half - 1, 0), (half + 1, 0), (half - 1, width - 1), (half + 1, width - 1)),
+    )
+    lti, rti, lbi, rbi = map(
+        count_tiles_in_plot,
+        ((half - 1, 1), (half + 1, 1), (half - 1, width - 2), (half + 1, width - 2)),
+    )
+    m1, m2 = map(count_tiles_in_plot, ((half, 1), (half, 2)))
+
+    print(t, l, r, b, lto, rto, lbo, rbo, lti, rti, lbi, rbi, m1, m2)
+
+    half = (26501365 - 65) / 131
+
+    return (
+        t
+        + l
+        + r
+        + b
+        + half * (lto + rto + lbo + rbo)
+        + (half - 1) * (lti + rti + lbi + rbi)
+        + pow(half, 2) * m1
+        + pow((half - 1), 2) * m2
+    )
 
 
 def main():
