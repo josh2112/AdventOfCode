6c68428b9cfa69df913abccef73f59b8f4168220
Joshua Foster <josh2112@gmail.com>
2024-01-10 13:06:05 -0500

    minor change to runall.py

--- 2023/runall.py
+++ 2023/runall.py
@@ -4,6 +4,7 @@ import os
 import sys
 
 
+# Go into each daily directory, import and run the solve script with "-p all" to do both parts.
 def main():
     total_time = 0.0
     base = os.getcwd()
@@ -13,14 +14,11 @@ def main():
     # These need work!
     skip = (5, 17, 18, 21, 25)
 
-    for d in range(1, 26):
-        if d in skip:
-            continue
-        day = str(d)
+    for day in [d for d in range(1, 26) if d not in skip]:
         print(f"--------[ Day {day} ]--------")
-        os.chdir(os.path.join(base, day))
-        script = __import__(f"{day}.solve")
 
+        os.chdir(os.path.join(base, str(day)))
+        script = __import__(f"{day}.solve")
         total_time += script.solve.main()
 
     print(f"--------[ Total time: {total_time} s ]--------")
