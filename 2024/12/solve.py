01e929afc73f13cef6485a41a3118414099e09f7
Joshua Foster <josh2112@gmail.com>
2024-12-12 09:07:10 -0500

    2024 day 12 part 1, working on part 2

---
+++ 2024/12/solve.py
@@ -0,0 +1,93 @@
+"""https://adventofcode.com/2024/day/12"""
+
+import argparse
+import time
+
+# Input file path (default is "input.txt")
+INPUT = "input.ex1.txt"
+
+# Part to solve, 1 or 2
+PART = 1
+
+
+def parse(data: list[str]):
+    border = [(".", -1)]
+    return [
+        border * (len(data[0]) + 2),
+        *[border + [[p, -1] for p in line] + border for line in data],
+        border * (len(data[0]) + 2),
+    ]
+
+
+def griditer(xstart, xstop, ystart, ystop):
+    for y in range(ystart, ystop):
+        for x in range(xstart, xstop):
+            yield x, y
+
+
+def flood_fill(garden, x, y, pid):
+    ltr = garden[y][x][0]
+    q = [(x, y)]
+    area, perimeter = 0, 0
+    while q:
+        p = q.pop(0)
+        area += 1
+        garden[p[1]][p[0]][1] = pid
+        neighbors = [
+            (p[0] + d[0], p[1] + d[1]) for d in ((-1, 0), (1, 0), (0, -1), (0, 1))
+        ]
+        perimeter += 4 - sum(
+            1 if garden[n[1]][n[0]][0] == ltr else 0 for n in neighbors
+        )
+        for n in neighbors:
+            if n not in q and garden[n[1]][n[0]] == [ltr, -1]:
+                q.append(n)
+    # print(f"{ltr} {(x,y)}: perim {perimeter}, area {area}")
+    return area, perimeter
+
+
+def segment(garden):
+    num_plots = 0
+    price = 0
+    for x, y in griditer(1, len(garden[0]) - 1, 1, len(garden) - 1):
+        if garden[y][x][1] == -1:
+            a, p = flood_fill(garden, x, y, num_plots)
+            price += a * p
+            num_plots += 1
+    return price
+
+
+def prob_1(data: list[str]) -> int:
+    garden = parse(data)
+    return segment(garden)
+
+
+def prob_2(data: list[str]) -> int:
+    print(data)
+    return 0
+
+
+def main() -> float:
+    parser = argparse.ArgumentParser(description="Solves AoC 2024 day 12.")
+    parser.add_argument("-p", "--part", choices=("1", "2", "all"), default=str(PART))
+    parser.add_argument("-i", "--input", default=INPUT)
+    args = parser.parse_args()
+    part, infile = args.part, args.input
+
+    with open(infile, mode="r", encoding="utf-8") as f:
+        data = [line.strip() for line in f.readlines()]
+
+    start = time.perf_counter()
+    if part in ("1", "all"):
+        print(f"Part 1: {prob_1(data)}")
+    if part in ("2", "all"):
+        print(f"Part 2: {prob_2(data)}")
+
+    elapsed = time.perf_counter() - start
+    print(f"Time: {elapsed} s")
+
+    return elapsed
+
+
+if __name__ == "__main__":
+    main()
