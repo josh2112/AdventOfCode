63cdacbc6eed96bb0f550fc5a725157b5c7c1f33
Joshua Foster <josh2112@gmail.com>
2025-01-09 16:07:01 -0500

    think I finally got 2022 day 19 part 2???

--- 2022/19/solve.py
+++ 2022/19/solve.py
@@ -1,16 +1,16 @@
 """https://adventofcode.com/2022/day/19"""
 
 import argparse
-import time
-import re
 import math
+import re
+import time
 from dataclasses import dataclass, field
 
 # Input file path (default is "input.txt")
 INPUT = "input.txt"
 
 # Part to solve, 1 or 2
-PART = 2
+PART = 1
 
 ORE, CLAY, OBS, GEO = 0, 1, 2, 3
 
@@ -42,35 +42,41 @@ def parse(data: list[str]) -> list[Blueprint]:
 def run(b: Blueprint, time_rem: int, bots: list[int], res: list[int]):
     # For each of the 4 robot types, figure out how long until we can build
     # another one. If we have time, do it.
-    # Ex: clay robot costs 5 ore. We have 2 ore and 2 ore robots. So we need 3 ore. which will take
-    # 2 robots ceil(3/2) = 2 seconds to produce.
+    # Ex: clay robot costs 5 ore. We have 2 ore and 2 ore robots, so we need 3 more ore, which will take
+    # the 2 robots ceil(3/2) = 2 seconds to produce.
 
     def project_resources(elapsed: int, costs: list[int]) -> list[int]:
         # Calculate resources after 'elapsed' seconds and spending 'costs'"
         return [r + b * elapsed - c for r, b, c in zip(res, bots, costs)]
 
+    def project_time_to_next_bot(cost, inv, num_bots):
+        if inv >= cost:
+            return 0
+        else:
+            return max(math.ceil((cost - inv) / num_bots), -1)
+
     # Calculate time until we can build, setting to -1 if we don't have
     # any bots of a certain type we need, or we already have enough bots of that type.
     next_bot_times = [
         # Ore
-        max(math.ceil((b.orebot_ore_cost - res[ORE]) / bots[ORE]), -1)
+        project_time_to_next_bot(b.orebot_ore_cost, res[ORE], bots[ORE])
         if bots[ORE] < b.max_ore_cost
         else -1,
         # Clay
-        max(math.ceil((b.claybot_ore_cost - res[ORE]) / bots[ORE]), -1)
+        project_time_to_next_bot(b.claybot_ore_cost, res[ORE], bots[ORE])
         if bots[CLAY] < b.obsbot_clay_cost
         else -1,
         # Obsidian
         max(
-            max(math.ceil((b.obsbot_ore_cost - res[ORE]) / bots[ORE]), -1),
-            max(math.ceil((b.obsbot_clay_cost - res[CLAY]) / bots[CLAY]), -1),
+            project_time_to_next_bot(b.obsbot_ore_cost, res[ORE], bots[ORE]),
+            project_time_to_next_bot(b.obsbot_clay_cost, res[CLAY], bots[CLAY]),
         )
         if bots[CLAY] and bots[OBS] < b.geobot_obs_cost
         else -1,
         # Geode
         max(
-            max(math.ceil((b.geobot_ore_cost - res[ORE]) / bots[ORE]), -1),
-            max(math.ceil((b.geobot_obs_cost - res[OBS]) / bots[OBS]), -1),
+            project_time_to_next_bot(b.geobot_ore_cost, res[ORE], bots[ORE]),
+            project_time_to_next_bot(b.geobot_obs_cost, res[OBS], bots[OBS]),
         )
         if bots[OBS]
         else -1,
@@ -82,10 +88,6 @@ def run(b: Blueprint, time_rem: int, bots: list[int], res: list[int]):
     # Don't build any more ore bots 2 seconds from the end
     if next_bot_times[ORE] and next_bot_times[ORE] > time_rem - 2:
         next_bot_times[ORE] = None
-
-    # print(
-    #    f"{' ' * (4 - t)} At t={4-t}: bots: {bots} res: {res} times to next bot: {next_bot_times}"
-    # )
 
     if not any(next_bot_times):
         # No more robots can be built? Project & return geode count at t=0
@@ -148,12 +150,7 @@ def prob_1(data: list[str]) -> int:
 
 
 def prob_2(data: list[str]) -> int:
-    # 15990 (13, 30, 41) is too low??
-    # Apparently correct answer is (13,31,42)?
-    # For blueprint 2:
-    # My last robot is built at t=1 res: [28, 47, 10, 24] bots: [4, 8, 6, 6] to give 24 + 6 = 30 geodes
-    # Alt code res: [28, 46, 10, 25] bots: [4, 8, 6, 6] to give 25 + 6 = 31 geodes
-    return math.prod(geode_count(b, 32) for b in parse(data)[1:2])
+    return math.prod(geode_count(b, 32) for b in parse(data))
 
 
 def main() -> float:
