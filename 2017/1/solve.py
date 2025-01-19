cc6e724ee8278b5f9d9c0e92f22a3cd3e0fa4fff
Joshua Foster <josh2112@gmail.com>
2025-01-19 13:25:43 -0500

    slightly edited template; started on year 2017

--- 2017/1/solve.py
+++ 2017/1/solve.py
@@ -1,4 +1,4 @@
-"""https://adventofcode.com/$year/day/$day"""
+"""https://adventofcode.com/2017/day/1"""
 
 import argparse
 import time
@@ -10,18 +10,23 @@ INPUT = "input.txt"
 PART = 1
 
 
+def captcha(data: list[str], offset: int = 1) -> int:
+    """compares every number in the list to the number 'offset' away, sums the ones that match"""
+    lst = list(map(int, data))
+    w = len(lst)
+    return sum(lst[i] for i in range(w) if lst[(i + offset) % w] == lst[i])
+
+
 def prob_1(data: list[str]) -> int:
-    print(data)
-    return 0
+    return captcha(data[0], 1)
 
 
 def prob_2(data: list[str]) -> int:
-    print(data)
-    return 0
+    return captcha(data[0], len(data[0]) >> 1)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Solves AoC $year day $day.")
+    parser = argparse.ArgumentParser(description="Solves AoC 2017 day 1.")
     parser.add_argument("-p", "--part", choices=("1", "2", "all"), default=str(PART))
     parser.add_argument("-i", "--input", default=INPUT)
     args = parser.parse_args()
