8ef60dc39f6ea99a2971900209ffc51a14b861f9
Joshua Foster <josh2112@gmail.com>
2025-01-30 16:35:16 -0500

    2017 day 17 complete!

--- 2017/17/solve.py
+++ 2017/17/solve.py
@@ -4,10 +4,10 @@ import argparse
 import time
 
 # Input file path (default is "input.txt")
-INPUT = "input.txt"
+INPUT = "input.ex.txt"
 
 # Part to solve, 1 or 2
-PART = 1
+PART = 2
 
 
 def prob_1(data: list[str]) -> int:
@@ -23,24 +23,17 @@ def prob_1(data: list[str]) -> int:
 
 def prob_2(data: list[str]) -> int:
     stepsize = int(data[0])
-    buf, i = [0], 0
-    i_zero = 0
-    tgt = 0
+    i, iz = 1, 1
+    vz = 1
 
-    # Possible approach: We don't care about the contents of the list, we only care about
-    # the value after the 0. And that value resets to the current iteration every time an insert
-    # is made at its index. So can we store just the length of the list, the index of the post-0
-    # value, and the current value of it?
+    for v in range(2, 50_000_000):
+        i = (i + stepsize + 1) % v
+        if iz > i:
+            iz += 1
+        elif iz == i:
+            vz = v
 
-    for v in range(1, 5_000_001):
-        i = (i + stepsize + 1) % len(buf)
-        buf.insert(i, v)
-        if i_zero >= i:
-            i_zero += 1
-        # print([buf[x % len(buf)] for x in range(i_zero - 1, i_zero + 2)])
-        if not v % 10_000 and buf[(i_zero + 1) % len(buf)] != tgt:
-            tgt = buf[(i_zero + 1) % len(buf)]
-            print(tgt)
+    return vz
 
 
 if __name__ == "__main__":
