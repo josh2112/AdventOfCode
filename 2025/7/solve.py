169ffe89443e29fbd2999259bd8159581ad52fae
Joshua Foster <josh2112@gmail.com>
2025-12-09 11:58:09 -0500

    FINALLY solved 2025 day 7 part 2

--- 2025/7/solve.py
+++ 2025/7/solve.py
@@ -1,7 +1,7 @@
 """https://adventofcode.com/2025/day/7"""
 
+from collections import defaultdict
 from dataclasses import dataclass, field
-from functools import cache
 
 from aoclib.runner import solve
 
@@ -27,68 +27,18 @@ def prob_1(data: list[str]) -> int:
     return split_cnt
 
 
-def prob_2_old(data: list[str]) -> int:
-    timeline_cnt = 0
-
-    # TODO: Not fast enough - takes 6 seconds on 80 rows, but full input is 140
-
-    # Just for testing...
-    data = data[:50]
-
-    data.append("-" * len(data[0]))  # Sentinel row
-
-    @cache
-    def next_splitter(x: int, y: int) -> int:
-        while data[y][x] == ".":
-            y += 1
-        return -1 if data[y][x] == "-" else y
-
-    def trace(x: int, y: int):
-        # Find next splitter or sentinel
-        y = next_splitter(x, y)
-
-        if y == -1:
-            nonlocal timeline_cnt
-            timeline_cnt += 1
-            return
-
-        trace(x - 1, y + 1)
-        trace(x + 1, y + 1)
-
-    trace(data[0].index("S"), 1)
-
-    return timeline_cnt
-
-
 def prob_2(data: list[str]) -> int:
-    data.append("-" * len(data[0]))  # Sentinel row
+    # Start with a row of all 1's
+    # For each row from the bottom:
+    #  - Where ^ found, sum the two adjacents
 
-    @dataclass
-    class Node:
-        parent: "Node | None" = None
-        count: int = 0
+    counts = [1 for _ in range(len(data[0]))]
 
-    def build(x: int, y: int, parent: Node | None):
-        if (x, y) not in nodes.keys():
-            nodes[(x, y)] = Node(parent)
-        n = nodes[(x, y)]
+    for line in reversed(data):
+        for x in [x for x, c in enumerate(line) if c == "^"]:
+            counts[x] = counts[x - 1] + counts[x + 1]
 
-        while data[y][x] == ".":
-            y += 1
-
-        if data[y][x] == "^":
-            build(x - 1, y + 1, n)
-            build(x + 1, y + 1, n)
-
-    nodes: dict[tuple[int, int], Node] = {}
-
-    build(data[0].index("S"), 0, None)
-    print(nodes)
-
-    # TODO 1: Ensure tree built OK
-    # TODO 2: Trace from bottom (1 level at a time) incrementing count on parent node
-
-    return 0
+    return counts[data[0].index("S")]
 
 
 if __name__ == "__main__":
